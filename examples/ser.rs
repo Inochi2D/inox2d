d73a5e474a63503773df575ded7b6edd86ee3ea5
Speykious <speykious@gmail.com>
2022-11-20 00:51:59 +0100

    What am I doing with my git history? :(

--- examples/ser.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let node_id = unsafe { NodeId::new(727) };
-
-    let serialized = serde_json::to_string(&node_id).unwrap();
-    println!("serialized = {}", serialized);
-
-    let deserialized: NodeId = serde_json::from_str(&serialized).unwrap();
-    println!("deserialized = {:?}", deserialized);
-}
