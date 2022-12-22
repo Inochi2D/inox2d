7d6400f4fe8e6cba0f20b264d79a3a1700887844
Fiana Fortressia <fortressnordlys@outlook.com>
2022-12-22 12:14:12 +0700

    add nodes and textures to App::launch function
    Signed-off-by: Fiana Fortressia <fortressnordlys@outlook.com>

--- src/renderers/mod.rs
+++ src/renderers/mod.rs
@@ -1,4 +1,4 @@
-use crate::{nodes::node_tree::NodeTree, model::ModelTexture};
+use crate::{model::ModelTexture, nodes::node_tree::NodeTree};
 
 #[cfg(feature = "opengl")]
 pub mod opengl;
@@ -8,7 +8,11 @@ where
     Self::Error: std::error::Error,
 {
     fn update(&self, event: winit::event::Event<()>);
-    fn launch(window: &winit::window::Window, nodes: NodeTree, textures: Vec<ModelTexture>) -> Result<Self, Self::Error>
+    fn launch(
+        window: &winit::window::Window,
+        nodes: NodeTree,
+        textures: Vec<ModelTexture>,
+    ) -> Result<Self, Self::Error>
     where
         Self: Sized;
     type Error;
