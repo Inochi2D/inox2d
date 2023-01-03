16db5690322b1a0c183303c6042d44b08d1bd462
Luna <cli-luna@protonmail.com>
2023-01-03 10:57:19 +0100

    Improve code style

--- src/math/camera.rs
+++ src/math/camera.rs
@@ -9,9 +9,9 @@ pub struct Camera {
 impl Default for Camera {
     fn default() -> Self {
         Self {
-            position: Vec2{ x: 0.0, y: 0.0 },
+            position: Vec2::ZERO,
             rotation: 0.0,
-            scale: Vec2::ONE
+            scale: Vec2::ONE,
         }
     }
 }
@@ -29,7 +29,7 @@ impl Camera {
     /// Gets the center offset of the viewport
     pub fn get_center_offset(&mut self, viewport: Vec2) -> Vec2 {
         let real_size = self.get_real_size(viewport);
-        Vec2 { x: (real_size.x/2.0), y: (real_size.y/2.0) }
+        real_size/2.0
     }
 
     /// Gets the resulting matrix from the camera and viewport
@@ -37,12 +37,12 @@ impl Camera {
         let real_size = self.get_real_size(viewport);
 
         // Faster to reuse real_size, so do that instead of calling get_center_offset
-        let origin = Vec2{ x: real_size.x/2.0, y: real_size.y/2.0 };
-        let pos = Vec3{ x: self.position.x, y: self.position.y, z: -(u16::MAX as f32)/2.0};
+        let origin = real_size/2.0;
+        let pos = self.position.extend(-(u16::MAX as f32/2.0));
 
         // Return camera ortho matrix
         Mat4::orthographic_lh(0.0, real_size.x, real_size.y, 0.0, 0.0, u16::MAX as f32) * 
-            Mat4::from_translation(Vec3{x: origin.x, y: origin.y, z: 0.0}) *
+            Mat4::from_translation(origin.extend(0.0)) *
             Mat4::from_rotation_z(self.rotation) *
             Mat4::from_translation(pos)
     }
