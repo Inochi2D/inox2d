6b7bdb002aaa551e7bf9fccd75f59f916d13483b
linkmauve <linkmauve@linkmauve.fr>
2023-01-05 14:52:12 +0100

    Add missing license and copyright (#21)

--- src/texture/tga.rs
+++ src/texture/tga.rs
@@ -1,3 +1,9 @@
+// Copyright (c) 2022 Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 use super::Texture;
 
 #[inline]
