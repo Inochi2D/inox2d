f47dbf049d7abb5d334cef942cc2407b3cee127c
Speykious <speykious@gmail.com>
2023-01-10 12:21:28 +0100

    First step: tail renders :D

---
+++ src/renderers/opengl/mod.rs
@@ -0,0 +1,172 @@
+pub mod gl_buffer;
+pub mod shader;
+pub mod shaders;
+pub mod texture;
+
+use glam::{uvec2, UVec2, Vec2, Vec3};
+use glow::HasContext;
+
+use crate::math::camera::Camera;
+use crate::model::ModelTexture;
+use crate::nodes::node::InoxNodeUuid;
+use crate::nodes::node_data::InoxData;
+use crate::nodes::node_tree::InoxNodeTree;
+
+use self::gl_buffer::GlBuffer;
+use self::shader::ShaderCompileError;
+use self::shaders::PartShader;
+use self::texture::{Texture, TextureError};
+
+#[derive(Debug, thiserror::Error)]
+#[error("Could not initialize OpenGL renderer: {0}")]
+pub enum OpenglRendererError {
+    ShaderCompile(#[from] ShaderCompileError),
+    Opengl(String),
+}
+
+pub struct OpenglRenderer<T = ()> {
+    gl: glow::Context,
+    pub camera: Camera,
+    pub viewport: UVec2,
+
+    vao: glow::NativeVertexArray,
+
+    verts: GlBuffer<Vec2>,
+    uvs: GlBuffer<Vec2>,
+    deforms: GlBuffer<Vec2>,
+    indices: GlBuffer<u16>,
+
+    part_shader: PartShader,
+
+    textures: Vec<Texture>,
+
+    pub nodes: InoxNodeTree<T>,
+    pub sorted_uuids: Vec<InoxNodeUuid>,
+}
+
+impl<T> OpenglRenderer<T> {
+    pub fn new(
+        gl: glow::Context,
+        viewport: UVec2,
+        nodes: InoxNodeTree<T>,
+    ) -> Result<Self, OpenglRendererError> {
+        unsafe { gl.viewport(0, 0, viewport.x as i32, viewport.y as i32) };
+
+        let mut verts = GlBuffer::new();
+        let mut uvs = GlBuffer::new();
+        let mut indices = GlBuffer::new();
+
+        let sorted_uuids = nodes.zsorted();
+        for node_id in &sorted_uuids {
+            let node = nodes.get_node(*node_id).unwrap();
+
+            if let InoxData::Part(ref part) = node.data {
+                let mesh = &part.mesh;
+                verts.extend_from_slice(&mesh.vertices);
+                uvs.extend_from_slice(&mesh.uvs);
+                indices.extend_from_slice(&mesh.indices);
+            }
+        }
+
+        // Initialize deforms to 0
+        let deforms = GlBuffer::from(vec![Vec2::ZERO; verts.len()]);
+
+        // Initialize buffers
+        let vao;
+        unsafe {
+            vao = gl
+                .create_vertex_array()
+                .map_err(OpenglRendererError::Opengl)?;
+            gl.bind_vertex_array(Some(vao));
+
+            verts.upload(&gl, glow::ARRAY_BUFFER, glow::STATIC_DRAW);
+            gl.vertex_attrib_pointer_f32(0, 2, glow::FLOAT, false, 0, 0);
+            gl.enable_vertex_attrib_array(0);
+
+            uvs.upload(&gl, glow::ARRAY_BUFFER, glow::STATIC_DRAW);
+            gl.vertex_attrib_pointer_f32(1, 2, glow::FLOAT, false, 0, 0);
+            gl.enable_vertex_attrib_array(1);
+
+            deforms.upload(&gl, glow::ARRAY_BUFFER, glow::DYNAMIC_DRAW);
+            gl.vertex_attrib_pointer_f32(2, 2, glow::FLOAT, false, 0, 0);
+            gl.enable_vertex_attrib_array(2);
+
+            indices.upload(&gl, glow::ELEMENT_ARRAY_BUFFER, glow::STATIC_DRAW);
+        }
+
+        // Shaders
+        let part_shader = PartShader::new(&gl)?;
+
+        Ok(Self {
+            gl,
+            camera: Camera::default(),
+            viewport,
+
+            vao,
+
+            verts,
+            uvs,
+            deforms,
+            indices,
+
+            part_shader,
+
+            textures: Vec::new(),
+
+            nodes,
+            sorted_uuids,
+        })
+    }
+
+    pub fn upload_model_textures(
+        &mut self,
+        model_textures: &[ModelTexture],
+    ) -> Result<(), TextureError> {
+        for mtex in model_textures {
+            let tex = texture::Texture::new(&self.gl, mtex)?;
+            self.textures.push(tex);
+        }
+
+        Ok(())
+    }
+
+    pub fn resize(&mut self, x: u32, y: u32) {
+        self.viewport = uvec2(x, y);
+        unsafe { self.gl.viewport(0, 0, x as i32, y as i32) };
+    }
+
+    pub fn clear(&self) {
+        unsafe { self.gl.clear(glow::COLOR_BUFFER_BIT) };
+    }
+
+    pub fn draw_stuff(&self) {
+        let mvp = self.camera.matrix(self.viewport.as_vec2());
+
+        let first_node = self.nodes.get_node(self.sorted_uuids[0]).unwrap();
+        let InoxData::Part(ref part) = first_node.data else {
+            eprintln!("nope");
+            return;
+        };
+
+        let gl = &self.gl;
+        let part_shader = &self.part_shader;
+        unsafe {
+            part_shader.bind(gl);
+
+            // textures
+            self.textures[part.tex_albedo].bind_on(gl, 0);
+
+            // vert uniforms
+            part_shader.set_mvp(gl, mvp);
+            part_shader.set_offset(gl, first_node.transform.translation.truncate());
+
+            // frag uniforms
+            part_shader.set_opacity(gl, 1.0);
+            part_shader.set_mult_color(gl, Vec3::splat(1.0));
+            part_shader.set_screen_color(gl, Vec3::splat(0.0));
+
+            let count = part.mesh.indices.len();
+            gl.draw_elements(glow::TRIANGLES, count as i32, glow::UNSIGNED_SHORT, 0);
+        }
+    }
+}
---
+++ src/renderers/opengl/texture.rs
@@ -0,0 +1,119 @@
+use glow::HasContext;
+use image::{ImageBuffer, ImageError, Rgba};
+
+use crate::model::ModelTexture;
+
+#[derive(thiserror::Error, Debug)]
+pub enum TextureError {
+    #[error("Could not create texture: {0}")]
+    Create(String),
+    #[error("Could not load image data for texture: {0}")]
+    LoadData(ImageError),
+}
+
+pub struct Texture {
+    tex: glow::NativeTexture,
+    width: u32,
+    height: u32,
+    bpp: u32,
+}
+
+impl Texture {
+    pub fn new(gl: &glow::Context, mtex: &ModelTexture) -> Result<Self, TextureError> {
+        let img_buf = image::load_from_memory_with_format(&mtex.data, mtex.format)
+            .map_err(TextureError::LoadData)?
+            .into_rgba8();
+
+        Self::from_image_buffer_rgba(gl, img_buf)
+    }
+
+    /// Makes an educated guess about the image format. TGA is not supported by this function.
+    pub fn from_memory(gl: &glow::Context, img_buf: &[u8]) -> Result<Self, TextureError> {
+        let img_buf = image::load_from_memory(img_buf)
+            .map_err(TextureError::LoadData)?
+            .into_rgba8();
+
+        Self::from_image_buffer_rgba(gl, img_buf)
+    }
+
+    pub fn from_image_buffer_rgba(
+        gl: &glow::Context,
+        img_buf: ImageBuffer<Rgba<u8>, Vec<u8>>,
+    ) -> Result<Self, TextureError> {
+        let pixel_buf = img_buf.to_vec();
+        let width = img_buf.width();
+        let height = img_buf.height();
+        let bpp = 8 * (pixel_buf.len() / (width as usize * height as usize)) as u32;
+
+        let tex = unsafe { gl.create_texture().map_err(TextureError::Create)? };
+        unsafe {
+            gl.bind_texture(glow::TEXTURE_2D, Some(tex));
+            gl.tex_parameter_i32(
+                glow::TEXTURE_2D,
+                glow::TEXTURE_MIN_FILTER,
+                glow::LINEAR as i32,
+            );
+            gl.tex_parameter_i32(
+                glow::TEXTURE_2D,
+                glow::TEXTURE_MAG_FILTER,
+                glow::LINEAR as i32,
+            );
+            gl.tex_parameter_i32(
+                glow::TEXTURE_2D,
+                glow::TEXTURE_WRAP_S,
+                glow::CLAMP_TO_EDGE as i32,
+            );
+            gl.tex_parameter_i32(
+                glow::TEXTURE_2D,
+                glow::TEXTURE_WRAP_T,
+                glow::CLAMP_TO_EDGE as i32,
+            );
+            gl.tex_image_2d(
+                glow::TEXTURE_2D,
+                0,
+                glow::RGBA8 as i32,
+                width as i32,
+                height as i32,
+                0,
+                glow::RGBA,
+                glow::UNSIGNED_BYTE,
+                Some(pixel_buf.as_slice()),
+            );
+            gl.bind_texture(glow::TEXTURE_2D, None);
+        }
+
+        Ok(Texture {
+            tex,
+            width,
+            height,
+            bpp,
+        })
+    }
+
+    pub fn bind(&self, gl: &glow::Context) {
+        self.bind_on(gl, 0);
+    }
+
+    pub fn bind_on(&self, gl: &glow::Context, slot: u32) {
+        unsafe {
+            gl.active_texture(glow::TEXTURE0 + slot);
+            gl.bind_texture(glow::TEXTURE_2D, Some(self.tex));
+        }
+    }
+
+    pub fn unbind(&self, gl: &glow::Context) {
+        unsafe { gl.bind_texture(glow::TEXTURE_2D, None) };
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn bpp(&self) -> u32 {
+        self.bpp
+    }
+}
