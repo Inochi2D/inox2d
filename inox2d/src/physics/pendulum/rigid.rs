31a4c686540a70029c9049b250289d2abdae3c5f
Speykious <speykious@gmail.com>
2024-02-12 03:09:39 +0100

    Back to getters and setters after all... :(

--- inox2d/src/physics/pendulum/rigid.rs
+++ inox2d/src/physics/pendulum/rigid.rs
@@ -12,38 +12,38 @@ type RigidPendulumState = PhysicsState<2, RigidPendulum>;
 impl RigidPendulumState {
     // θ
 
-    pub fn vθ(&self) -> f32 {
+    pub fn get_vθ(&self) -> f32 {
         self.vars[0]
     }
 
-    pub fn vθ_mut(&mut self) -> &mut f32 {
-        &mut self.vars[0]
+    pub fn set_vθ(&mut self, value: f32) {
+        self.vars[0] = value;
     }
 
-    pub fn dθ(&self) -> f32 {
+    pub fn get_dθ(&self) -> f32 {
         self.derivatives[0]
     }
 
-    pub fn dθ_mut(&mut self) -> &mut f32 {
-        &mut self.derivatives[0]
+    pub fn set_dθ(&mut self, value: f32) {
+        self.derivatives[0] = value;
     }
 
     // ω
 
-    pub fn vω(&self) -> f32 {
+    pub fn get_vω(&self) -> f32 {
         self.vars[1]
     }
 
-    pub fn vω_mut(&mut self) -> &mut f32 {
-        &mut self.vars[1]
+    pub fn set_vω(&mut self, value: f32) {
+        self.vars[1] = value;
     }
 
-    pub fn dω(&self) -> f32 {
+    pub fn get_dω(&self) -> f32 {
         self.derivatives[1]
     }
 
-    pub fn dω_mut(&mut self) -> &mut f32 {
-        &mut self.derivatives[1]
+    pub fn set_dω(&mut self, value: f32) {
+        self.derivatives[1] = value;
     }
 }
 
@@ -54,17 +54,17 @@ fn eval(state: &mut RigidPendulumState, props: &SimplePhysicsProps, _anchor: Vec
     let r = props.length;
 
     // θ' = ω
-    *state.dθ_mut() = state.vω();
+    state.set_dθ(state.get_vω());
 
     // ω' = -(g/R) sin θ
-    let dω = -(g / r) * state.vθ().sin();
+    let dω = -(g / r) * state.get_vθ().sin();
 
     // critical damp: that way a damping value of 1 corresponds to no bouncing
     let crit_damp = 2. * (g / r).sqrt();
 
-    let damping = -state.vω() * props.angle_damping * crit_damp;
+    let damping = -state.get_vω() * props.angle_damping * crit_damp;
 
-    *state.dω_mut() = dω + damping;
+    state.set_dω(dω + damping);
 }
 
 #[derive(Debug, Clone, Default)]
@@ -77,13 +77,13 @@ impl RigidPendulumSystem {
     pub fn tick(&mut self, anchor: Vec2, props: &SimplePhysicsProps, dt: f32) -> Vec2 {
         // Compute the angle against the updated anchor position
         let d_bob = self.bob - anchor;
-        *self.state.vθ_mut() = f32::atan2(-d_bob.x, d_bob.y);
+        self.state.set_vθ(f32::atan2(-d_bob.x, d_bob.y));
 
         // Run the pendulum simulation in terms of angle
         runge_kutta::tick(&eval, &mut self.state, props, anchor, dt);
 
         // Update the bob position at the new angle
-        let angle = self.state.vθ();
+        let angle = self.state.get_vθ();
         let d_bob = vec2(-angle.sin(), angle.cos());
         self.bob = anchor + d_bob * props.length;
 
--- inox2d/src/physics/pendulum/spring.rs
+++ inox2d/src/physics/pendulum/spring.rs
@@ -1,4 +1,4 @@
-use crate::physics::runge_kutta::{PhysicsState, self};
+use crate::physics::runge_kutta::{self, PhysicsState};
 use crate::physics::SimplePhysicsProps;
 use glam::{vec2, Vec2};
 use std::f32::consts::PI;
@@ -8,50 +8,51 @@ struct SpringPendulum;
 
 type SpringPendulumState = PhysicsState<4, SpringPendulum>;
 
+#[allow(unused)]
 impl SpringPendulumState {
     // bob
 
-    pub fn getv_bob(&self) -> Vec2 {
+    pub fn get_vbob(&self) -> Vec2 {
         vec2(self.vars[0], self.vars[1])
     }
 
-    pub fn setv_bob(&mut self, bob: Vec2) {
+    pub fn set_vbob(&mut self, bob: Vec2) {
         self.vars[0] = bob.x;
         self.vars[1] = bob.y;
     }
 
-    // pub fn getd_bob(&self) -> Vec2 {
-    //     vec2(self.derivatives[0], self.derivatives[1])
-    // }
+    pub fn get_dbob(&self) -> Vec2 {
+        vec2(self.derivatives[0], self.derivatives[1])
+    }
 
-    pub fn setd_bob(&mut self, bob: Vec2) {
+    pub fn set_dbob(&mut self, bob: Vec2) {
         self.derivatives[0] = bob.x;
         self.derivatives[1] = bob.y;
     }
 
-    // dbob
+    // bobvel
 
-    pub fn getv_dbob(&self) -> Vec2 {
+    pub fn get_vbobvel(&self) -> Vec2 {
         vec2(self.vars[2], self.vars[3])
     }
 
-    // pub fn setv_dbob(&mut self, dbob: Vec2) {
-    //     self.vars[2] = dbob.x;
-    //     self.vars[3] = dbob.y;
-    // }
+    pub fn set_vbobvel(&mut self, dbob: Vec2) {
+        self.vars[2] = dbob.x;
+        self.vars[3] = dbob.y;
+    }
 
-    // pub fn getd_dbob(&self) -> Vec2 {
-    //     vec2(self.derivatives[2], self.derivatives[3])
-    // }
+    pub fn get_dbobvel(&self) -> Vec2 {
+        vec2(self.derivatives[2], self.derivatives[3])
+    }
 
-    pub fn setd_dbob(&mut self, dbob: Vec2) {
+    pub fn set_dbobvel(&mut self, dbob: Vec2) {
         self.derivatives[2] = dbob.x;
         self.derivatives[3] = dbob.y;
     }
 }
 
 fn eval(state: &mut SpringPendulumState, props: &SimplePhysicsProps, anchor: Vec2, _t: f32) {
-    state.setd_bob(state.getv_dbob());
+    state.set_dbob(state.get_vbobvel());
 
     // These are normalized vs. mass
     let spring_ksqrt = props.frequency * 2. * PI;
@@ -60,17 +61,17 @@ fn eval(state: &mut SpringPendulumState, props: &SimplePhysicsProps, anchor: Vec
     let g = props.gravity;
     let rest_length = props.length - g / spring_k;
 
-    let off_pos = state.getv_bob() - anchor;
+    let off_pos = state.get_vbob() - anchor;
     let off_pos_norm = off_pos.normalize();
 
-    let length_ratio = props.gravity / props.length;
+    let length_ratio = g / props.length;
     let crit_damp_angle = 2. * length_ratio.sqrt();
     let crit_damp_length = 2. * spring_ksqrt;
 
-    let dist = anchor.distance(state.getv_bob()).abs();
+    let dist = anchor.distance(state.get_vbob()).abs();
     let force = vec2(0., g) - (off_pos_norm * (dist - rest_length) * spring_k);
 
-    let d_bob = state.getv_dbob();
+    let d_bob = state.get_vbobvel();
     let d_bob_rot = vec2(
         d_bob.x * off_pos_norm.y + d_bob.y * off_pos_norm.x,
         d_bob.y * off_pos_norm.y - d_bob.x * off_pos_norm.x,
@@ -88,7 +89,7 @@ fn eval(state: &mut SpringPendulumState, props: &SimplePhysicsProps, anchor: Vec
 
     let dd_bob = force + dd_bob_damping;
 
-    state.setd_dbob(dd_bob);
+    state.set_dbobvel(dd_bob);
 }
 
 #[derive(Debug, Clone, Default)]
@@ -99,12 +100,12 @@ pub struct SpringPendulumSystem {
 
 impl SpringPendulumSystem {
     pub fn tick(&mut self, anchor: Vec2, props: &SimplePhysicsProps, dt: f32) -> Vec2 {
-        self.state.setv_bob(self.bob);
+        self.state.set_vbob(self.bob);
 
         // Run the spring pendulum simulation
         runge_kutta::tick(&eval, &mut self.state, props, anchor, dt);
 
-        self.bob = self.state.getv_bob();
+        self.bob = self.state.get_vbob();
 
         self.bob
     }
