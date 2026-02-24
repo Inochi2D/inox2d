1f48dea404f352b30b63b7234f860a07f227085f
Arcturus Emrys <aemrys@arcturus.live>
2026-02-24 23:25:28 +0000

    Cargo fmt the example wgpu renderer app

--- examples/render-wgpu/src/main.rs
+++ examples/render-wgpu/src/main.rs
@@ -1,19 +1,19 @@
-use env_logger;
-use log::*;
-use winit::event_loop::{EventLoop, ControlFlow};
-use winit::window::WindowBuilder;
-use winit::event::{Event, WindowEvent};
 use clap::Parser;
+use common::scene::ExampleSceneController;
+use env_logger;
 use inox2d::formats::inp::parse_inp;
 use inox2d::math::camera::Camera;
 use inox2d::render::InoxRendererExt;
 use inox2d_wgpu::WgpuRenderer;
+use log::*;
 use pollster::block_on;
-use common::scene::ExampleSceneController;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
 
 use std::error::Error;
-use std::path::PathBuf;
 use std::fs;
+use std::path::PathBuf;
 use std::sync::Arc;
 
 #[derive(Parser, Debug)]
@@ -24,57 +24,68 @@ struct Cli {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let cli = Cli::parse();
+	let cli = Cli::parse();
 
-    env_logger::init();
+	env_logger::init();
 
-    let event_loop = EventLoop::new()?;
+	let event_loop = EventLoop::new()?;
 
-    info!("Loading {:?}", cli.inp_path);
+	info!("Loading {:?}", cli.inp_path);
 
-    let data = fs::read(cli.inp_path)?;
-    let mut model = parse_inp(data.as_slice())?;
+	let data = fs::read(cli.inp_path)?;
+	let mut model = parse_inp(data.as_slice())?;
 
-    info!("Successfully parsed puppet: {}", 
-		(model.puppet.meta.name.as_deref()).unwrap_or("<no puppet name specified in file>"));
-    
-    model.puppet.init_transforms();
-    model.puppet.init_rendering();
-    model.puppet.init_params();
-    model.puppet.init_physics();
+	info!(
+		"Successfully parsed puppet: {}",
+		(model.puppet.meta.name.as_deref()).unwrap_or("<no puppet name specified in file>")
+	);
 
-    let window = Arc::new(WindowBuilder::new().build(&event_loop).expect("valid window"));
-    let mut renderer = block_on(WgpuRenderer::new(window.clone(), &model)).expect("valid renderer");
-    let mut scene_controller = ExampleSceneController::new(&Camera::default(), 0.5);
-    let camera = Camera::default();
+	model.puppet.init_transforms();
+	model.puppet.init_rendering();
+	model.puppet.init_params();
+	model.puppet.init_physics();
 
-    event_loop.set_control_flow(ControlFlow::Poll);
-    event_loop.run(|event, event_loop| {
-        match event {
-            Event::WindowEvent { event: WindowEvent::Resized(new_size), .. } => {
-                if let Err(err) = renderer.resize(new_size.width, new_size.height) {
-                    error!("Resize failed: {}", err);
-                }
-            }
-            Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
-                event_loop.exit();
-            }
-            Event::WindowEvent { event: WindowEvent::RedrawRequested, .. } => {
-                if let Err(err) = renderer.draw(&model.puppet) {
-                    error!("Draw failed: {}", err);
-                }
-            }
-            Event::WindowEvent { event, .. } => {
-                scene_controller.interact(&event, &camera);
-            },
-            Event::AboutToWait => {
-                window.request_redraw();
+	let window = Arc::new(WindowBuilder::new().build(&event_loop).expect("valid window"));
+	let mut renderer = block_on(WgpuRenderer::new(window.clone(), &model)).expect("valid renderer");
+	let mut scene_controller = ExampleSceneController::new(&Camera::default(), 0.5);
+	let camera = Camera::default();
 
-                //TODO: Swapchain? Swapchain.
-            }
-            _ => {}
-        }
-    })?;
+	event_loop.set_control_flow(ControlFlow::Poll);
+	event_loop.run(|event, event_loop| {
+		match event {
+			Event::WindowEvent {
+				event: WindowEvent::Resized(new_size),
+				..
+			} => {
+				if let Err(err) = renderer.resize(new_size.width, new_size.height) {
+					error!("Resize failed: {}", err);
+				}
+			}
+			Event::WindowEvent {
+				event: WindowEvent::CloseRequested,
+				..
+			} => {
+				event_loop.exit();
+			}
+			Event::WindowEvent {
+				event: WindowEvent::RedrawRequested,
+				..
+			} => {
+				if let Err(err) = renderer.draw(&model.puppet) {
+					error!("Draw failed: {}", err);
+				}
+			}
+			Event::WindowEvent { event, .. } => {
+				scene_controller.interact(&event, &camera);
+			}
+			Event::AboutToWait => {
+				window.request_redraw();
 
-    Ok(())
+				//TODO: Swapchain? Swapchain.
+			}
+			_ => {}
+		}
+	})?;
+
+	Ok(())
 }
