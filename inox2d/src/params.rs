59205fb6d4d58f27a94b624fb53069ffe70bc47c
Arcturus Emrys <aemrys@arcturus.live>
2026-03-09 23:20:02 +0000

    Well, ok, the screenTint is also multiplied I guess?

--- inox2d/src/params.rs
+++ inox2d/src/params.rs
@@ -265,19 +265,19 @@ impl Param {
 				BindingValues::ScreenTintR(ref matrix) => {
 					let (out_top, out_bottom) = ranges_out(matrix, x_mindex, x_maxdex, y_mindex, y_maxdex);
 
-					comps.get_mut::<Drawable>(binding.node).unwrap().blending.screen_tint.x *=
+					comps.get_mut::<Drawable>(binding.node).unwrap().blending.screen_tint.x +=
 						bi_interpolate_f32(val_normed, range_in, out_top, out_bottom, binding.interpolate_mode);
 				}
 				BindingValues::ScreenTintG(ref matrix) => {
 					let (out_top, out_bottom) = ranges_out(matrix, x_mindex, x_maxdex, y_mindex, y_maxdex);
 
-					comps.get_mut::<Drawable>(binding.node).unwrap().blending.screen_tint.y *=
+					comps.get_mut::<Drawable>(binding.node).unwrap().blending.screen_tint.y +=
 						bi_interpolate_f32(val_normed, range_in, out_top, out_bottom, binding.interpolate_mode);
 				}
 				BindingValues::ScreenTintB(ref matrix) => {
 					let (out_top, out_bottom) = ranges_out(matrix, x_mindex, x_maxdex, y_mindex, y_maxdex);
 
-					comps.get_mut::<Drawable>(binding.node).unwrap().blending.screen_tint.z *=
+					comps.get_mut::<Drawable>(binding.node).unwrap().blending.screen_tint.z +=
 						bi_interpolate_f32(val_normed, range_in, out_top, out_bottom, binding.interpolate_mode);
 				}
 				BindingValues::Opacity(ref matrix) => {
